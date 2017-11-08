bebb1023117eca107a8f1717cb9ed6ebea4a3cc7
ashneeldas2 <adas2@stuy.edu>
2017-11-08 14:31:50 -0500

    done

---
+++ signal.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+static void sighandler(int signo) {
+    if (signo == SIGINT) {
+        printf("I've been interrupted!! oh no!!!\n");
+        exit(1);
+    }
+
+    if (signo == SIGUSR1) {
+        printf("The parent process number is: %d\n", getppid());
+    }
+}
+
+int main() {
+    signal(SIGINT, sighandler);
+    signal(SIGUSR1, sighandler);
+
+    while (1) {
+        printf("The process number is: %d\n", getpid());
+        sleep(1);
+    }
+}
